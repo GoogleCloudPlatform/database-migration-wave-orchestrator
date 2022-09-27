ce3726cf762c82c74c673790cea90f1a7ede5530
Abel Tamrat <atamrat@google.com>
2022-09-27 14:15:07 +0000

    Simplifying deployment

--- deploy/gcp-foundation/modules/get-iap-brand.bash
+++
@@ -1,5 +0,0 @@
-eval "$(jq -r '@sh "PROJECT_ID=\(.project_id)"')"
-
-gcloud services enable iap.googleapis.com --project ${PROJECT_ID}
-BRAND_NAME=$(gcloud beta iap oauth-brands list --project=${PROJECT_ID} --format="value(name)")
-jq --null-input --arg brand "$BRAND_NAME" '{"brand": $brand}'
